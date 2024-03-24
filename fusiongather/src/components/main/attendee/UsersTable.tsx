ad307e857440e368d001652c200d9ec002250e65
Thanh <hondondaide@gmail.com>
2024-03-24 09:00:47 +0700

    add search attendee by name or email

--- fusiongather/src/components/main/attendee/UsersTable.tsx
+++ fusiongather/src/components/main/attendee/UsersTable.tsx
@@ -18,65 +18,121 @@ import {
   ModalContent,
   ModalFooter,
   ModalBody,
+  Input,
 } from "@nextui-org/react";
-import { use, useEffect, useMemo, useState } from "react";
+import { use, useCallback, useEffect, useMemo, useState } from "react";
 import { deleteAttendee, getAttendeeByEventId } from "@/lib/actions/attendee";
 import { set } from "zod";
-import {
-  DeleteIcon,
-  EditIcon,
-  EyeIcon,
-} from "@/app/(main)/event/attendeeList/icons";
 import { Modal, Button } from "@nextui-org/react";
+import { MdOutlineRemoveRedEye } from "react-icons/md";
+import { IoTrashOutline } from "react-icons/io5";
+import { SearchIcon } from "lucide-react";
+import { get } from "http";
 
 interface Props {
   ticket: Ticket[];
 }
 
-export default function UsersTable() {
+export default function UsersTable({tickets}: {tickets: Ticket[]}) {
   const [showModal, setShowModal] = useState(false);
   const [page, setPage] = useState(1);
   const [ticket, setTicket] = useState<Ticket[]>([]);
   const [isDelete, setIsDelete] = useState(0);
   const [deleteItemId, setDeleteItemId] = useState<number | null>(null);
+  const [filterValue, setFilterValue] = useState('')
+  const hasSearchFilter = Boolean(filterValue)
   const rowsPerPage = 4;
 
+  // get all attendees
   useEffect(() => {
     getAttendee();
   }, [isDelete]);
 
   async function getAttendee() {
-    const res = await getAttendeeByEventId(1);
+    const res = await getAttendeeByEventId(10);
     setTicket(res);
   }
-
+  // handle delete attendee
   const handleDeleteAttendee = (id: number) => {
     setDeleteItemId(id);
     setShowModal(true);
   };
 
+  // handle confirm delete
   const handleConfirmDelete = async () => {
     if (deleteItemId !== null) {
       await deleteAttendee(deleteItemId);
       setIsDelete(isDelete + 1);
     }
+    setDeleteItemId(null);
     setShowModal(false);
   };
 
+  // filter items out based on search value
+  const filterItems = useMemo(() => {
+    let filteredTickets = [...ticket];
+    // if search value is empty, return all items
+    if(hasSearchFilter){
+      filteredTickets = filteredTickets.filter(ticket => 
+        ticket.userId.firstName.toLowerCase().includes(filterValue.toLowerCase()) ||
+        ticket.userId.lastName.toLowerCase().includes(filterValue.toLowerCase()) ||
+        ticket.userId.email.toLowerCase().includes(filterValue.toLowerCase())
+      );
+    }
+    return filteredTickets;
+  }, [ticket, filterValue, hasSearchFilter]);
+
+  // calculate the number of pages
   const pages = Math.ceil(ticket.length / rowsPerPage);
-  const items = useMemo(() => {
+  // get the items to display on the current page
+  const items: Ticket[] = useMemo(() => {
     const start = (page - 1) * rowsPerPage;
     const end = start + rowsPerPage;
-    if (ticket.length > 0) {
-      return ticket.slice(start, end);
+    if (filterItems.length > 0) {
+      return filterItems.slice(start, end);
     }
     return [];
-  }, [page, ticket]);
+  }, [page, filterItems]);
 
+  // handle search value change
+  const onSearchChange = useCallback((value?: string) => {
+    if(value) {
+      setFilterValue(value);
+      setPage(1);
+    } else {
+      setFilterValue('');
+    }
+  }, [])
+
+  // handle clear search value
+  const onClear = useCallback(() => {
+    setFilterValue('');
+    setPage(1);
+  }, []);
+
+  const topContent = useMemo(() => {
+    return (
+      <div className="flex flex-col gap-4">
+        <div className="flex items-end justify-between gap-3" >
+          <Input
+            isClearable
+            className="w-full sm:max-w-[44%]"
+            placeholder="Search by name or email"
+            startContent={<SearchIcon />}
+            value={filterValue}
+            onClear={() => onClear()}
+            onValueChange={onSearchChange}
+          />
+        </div>
+      </div>
+    )
+  }, [filterValue, onSearchChange, onClear]);
   return (
     <>
       <Table
         aria-label="Example table with dynamic content"
+        topContent={topContent}
+        topContentPlacement="outside"
         bottomContent={
           <div className="flex w-full justify-center">
             <Pagination
@@ -109,12 +165,12 @@ export default function UsersTable() {
                     <div className="relative flex items-center gap-2">
                       <Tooltip content="Details">
                         <span className="text-lg text-default-400 cursor-pointer active:opacity-50">
-                          <EyeIcon />
+                          <MdOutlineRemoveRedEye />
                         </span>
                       </Tooltip>
                       <Tooltip color="danger" content="Delete Attendee">
                         <span className="text-lg text-danger cursor-pointer active:opacity-50">
-                          <DeleteIcon
+                          <IoTrashOutline
                             onClick={() => handleDeleteAttendee(item.id)}
                           />
                         </span>
